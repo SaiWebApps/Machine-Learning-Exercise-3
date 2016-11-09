22ed672ed2e038574f185f3fe46ca287ffa1042c
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-11-08 18:53:34 -0800

    Implemented Neural-Network prediction algorithm.
    Improved version of predictOneVsAll since neural networks support
    non-linear hypotheses, which allows for more accurate conclusions with
    large datasets (large in terms of # of features).

--- predict.m
+++ predict.m
@@ -3,33 +3,20 @@ function p = predict(Theta1, Theta2, X)
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
 
-% Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+m = size(X, 1); % # of training examples
+num_labels = size(Theta2, 1); % # of output classes
+X = [ones(m,1), X]; % Add column of 1s for X0 to input X.
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+hiddenLayer = sigmoid(X * Theta1'); % (5000x401) * (401x25) = (5000x25)
+% Add activation unit 0 (+1) for each training example.
+hiddenLayer = [ones(m, 1), hiddenLayer]; % (5000x26)
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
+% Each element (i,j) represents the probability that
+% training example i is label j.
+outputLayer = sigmoid(hiddenLayer * Theta2'); % (5000x26) * (26x10) = (5000x10)
 
-
-
-
-
-
-
-
-
-% =========================================================================
-
+% Now, find the label corresponding to the maximum probability for
+% each training example; this is our prediction for each example.
+[maxProbabilities, p] = max(outputLayer, [], 2); % p -> 5000x1
 
 end
